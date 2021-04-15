f4df963dedf7a3a60824e3548af9864f2cc8e111
darkbenq3d4 <davidelfuerte2010@gmail.com>
2021-04-15 12:10:20 -0400

    Changing to Typescript

--- scrapper.ts
+++ scrapper.ts
@@ -3,11 +3,11 @@ const firebase = require('firebase/app');
 const { database } = require('firebase/app');
 require('firebase/database');
 require('firebase/auth');
-let env = require('./env');
-firebase.initializeApp(env.firebaseConfig);
+import { firebaseConfig, user_email, user_password } from "./env";
+firebase.initializeApp(firebaseConfig);
 var DB = firebase.database();
 var Auth = firebase.auth();
-async function scrapeShit(url){
+async function scrapeShit(url:string){
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
@@ -16,7 +16,12 @@ async function scrapeShit(url){
     const provincias = await page.evaluate(() => {
       const rows = document.querySelectorAll('g#Republica-Dominicana g path');
         return Array.from(rows, row => 
-          ({nombre: row.getAttribute('data-original-title'), casos:row.getAttribute('data-content').split('<br>')[0].replace('Confirmados:',''), recuperados:row.getAttribute('data-content').split('<br>')[1].replace('Recuperados:',''), muertos:row.getAttribute('data-content').split('<br>')[2].replace('Fallecidas:','') }));
+          ({
+            nombre: row.getAttribute('data-original-title'),
+            casos: row.getAttribute('data-content')!.split('<br>')[0].replace('Confirmados:',''),
+            recuperados:row.getAttribute('data-content')!.split('<br>')[1].replace('Recuperados:',''),
+            muertos:row.getAttribute('data-content')!.split('<br>')[2].replace('Fallecidas:','') 
+          }));
     })
 
       
@@ -26,19 +31,9 @@ async function scrapeShit(url){
 }
 var Data = scrapeShit('https://coronavirusrd.gob.do/');
 Data.then(function(result){
-  Auth.signInWithEmailAndPassword("admin123012@gmail.com", "@EagleSystem20").then(user=>{
+  Auth.signInWithEmailAndPassword(user_email, user_password).then((user: any)=>{
     var i = 0;
-    result.forEach(dato => {
-      if(dato.nombre == "Bahoruco"){
-       DB.ref('DatosCovid19/' + i).set({
-         provincia : "Baoruco",
-         casos: dato.casos,
-         recuperados: dato.recuperados,
-         muertos: dato.muertos
-         }, () => {
-           console.log("Listo con Bahoruco");
-         });
-      }else{
+    result.forEach((dato: { nombre: string; casos: any; recuperados: any; muertos: any; }) => {
         DB.ref('DatosCovid19/' + i).set({
        provincia : dato.nombre,
        casos: dato.casos,
@@ -47,7 +42,6 @@ Data.then(function(result){
        }, () => {
         console.log("Listo con "+dato.nombre);
       });
-      }
        i++;
 
     });
